f3ec873c522f18db925c356f23b8212e0df6e8d6
Brandon Hahn <brandonmhahn@gmail.com>
2013-02-26 09:00:01 -0600

    added status validations and tests

--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "that a status requires content" do
+  	status = Status.new
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+  test "that a status's content is at least 2 characters long" do
+  	status = Status.new
+  	status.content = "O"
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+  test "that a status has a user associated with it" do
+  	status = Status.new
+  	status.content = "Hello world"
+  	assert !status.save
+  	assert !status.errors[:user_id].empty?
+  end
 end
